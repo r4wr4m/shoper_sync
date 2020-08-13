f2acafbe51e1c627c5d3030f1147c7a184e0bf5c
r4wr4m <61500192+r4wr4m@users.noreply.github.com>
2020-08-13 11:26:27 +0200

    added email credentials

--- creds.py
+++ creds.py
@@ -2,3 +2,7 @@ pages=[
         ['domain','user','pass','token'], #SHOP1
         ['','','',''] #SHOP2
       ]
+
+#e-mail credentials
+mail_creds=['mail@gmail.com','password']
+to = ['mail2@gmail.com'] #address for sending notifications
