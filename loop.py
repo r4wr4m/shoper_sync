e45027ea72cd3a461c90a8c32fd016dd5c25e060
r4wr4m <marwaraksa@gmail.com>
2020-08-14 12:12:22 +0200

    added loop.py, modified .gitignore

---
+++ loop.py
@@ -0,0 +1,17 @@
+import os,time
+logpath='log/'
+
+interval=5 #minutes
+iteration=0
+
+while True:
+    t = time.localtime()
+    minute = int(time.strftime('%M',t))
+    if minute%interval == 0:
+        iteration+=1
+        if not os.path.isdir(logpath):
+            os.mkdir(logpath)
+        filename = logpath + time.strftime('%Y%m%d_%H%M_'+str(iteration).zfill(5)+'.log',t)
+        #os.system('python3 sync_stock.py change>'+filename)
+        os.system('date>'+filename)
+    time.sleep(60)
