7e0048db9be0f3415c0a3e633208e06457f0da82
r4wr4m <marwaraksa@gmail.com>
2020-09-17 14:07:56 +0200

    updated

--- creds.py
+++ creds.py
@@ -1,10 +1,29 @@
 pages=[
-        #EXAMPLE: ['shopname.pl','admin','SuperSecretPassword',''],
-        ['domain','user','pass','token'], #SHOP1
-        ['','','',''] #SHOP2
+        [	#SHOP1
+        '',	#domain (ex. google.com)
+        '',	#user	
+        '', #password
+        '', #placeholder for token
+        '',	#Client ID (https://apps.developer.allegro.pl/)
+        '',	#Client Secret (https://apps.developer.allegro.pl/)
+        '',	#placeholder for Access Token
+        '',	#placeholder for Refresh Token
+        '',	#placeholder for expiration timestamp
+		],
+        [	#SHOP2
+        '',	#domain (ex. google.com)
+        '',	#user	
+        '', #password
+        '', #placeholder for token
+        '',	#Client ID (https://apps.developer.allegro.pl/)
+        '',	#Client Secret (https://apps.developer.allegro.pl/)
+        '',	#placeholder for Access Token
+        '',	#placeholder for Refresh Token
+        '',	#placeholder for expiration timestamp
+		],
       ]
 
 #e-mail credentials
-mail_creds=['mail@gmail.com','password']
-to = ['mail2@gmail.com'] #address for sending notifications
+mail_creds=['mail@gmail.com','password'] #gmail account for sending notifications 
+to = ['mail2@gmail.com'] #destination email address for sending notifications
 
