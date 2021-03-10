cd8cdb21a6349a668a68c407207034e8e1f05997
r4wr4m <61500192+r4wr4m@users.noreply.github.com>
2021-03-10 19:34:13 +0100

    Update products2file.py

--- products2file.py
+++ products2file.py
@@ -15,12 +15,14 @@ start = time.time()
 products,availabilities,deliveries,name_dict1,name_dict2,auctions = load_data(from_file,active_only) #load_data(active_only=False,from_file=False)
 
 if compare_products(products[0],products[1],name_dict1,name_dict2,pages[0][0],pages[1][0],availabilities[0],availabilities[1],deliveries[0],deliveries[1],[],False,False):
-	product_names=[]
-	for product in products[0]:
-		product_names.append(product['name'])
-	save_columns2xls([product_names],filename)
-	print(Fore.GREEN+'[+] File {}.xls saved.'.format(filename))
+    product_names=[]
+    passports=[]
+    for product in products[0]:
+        product_names.append(product['name'])
+        passports.append(product['passport'])
+    save_columns2xls([product_names,passports],filename)
+    print(Fore.GREEN+'[+] File {}.xls saved.'.format(filename))
 else:
-	print(Fore.RED+'[-] Different products in shops')
+    print(Fore.RED+'[-] Different products in shops')
 
 print('###################\nDone in {} seconds.'.format(round(time.time()-start,3)))
