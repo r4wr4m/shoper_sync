d7a743efedcdc05654d3b3336bd4efd5cf64d792
r4wr4m <61500192+r4wr4m@users.noreply.github.com>
2021-10-27 17:01:30 +0200

    Update orders2file.py

--- orders2file.py
+++ orders2file.py
@@ -35,10 +35,9 @@ if not page:
     print(Fore.RED+'[!] Page {} not found!'.format(domain))
     sys.exit(1)
 
-
 token = api_login(page[0],page[1],page[2])
 
-table_rows=[['NAZWA ODBIORCY','NAZWA ROŚLINY','DATA WYSYŁKI','PASZPORT']]
+table_rows=[['NAZWA ODBIORCY','NAZWA ROŚLINY','PASZPORT','CENA','ILOŚĆ','JEDNOSTKA','STATUS','DATA ZMIANY STATUSU','DATA DOSTAWY','INFO']]
 orders = get_orders(page[0],token,date_from,date_to)
 for order in orders:
     clientname=''
@@ -48,19 +47,29 @@ for order in orders:
         if clientname != '':
             clientname += ' | '
         clientname += order['firstname'] + ' ' + order['lastname']
+    status_date=order['status_date']
+    confirm_date=order['confirm_date']
+    delivery_date=order['delivery_date']
+    status_id=order['status']
+    
     ordered_products = get_ordered_products(page[0],token,order['order_id'])
     for product in ordered_products:
+        price=product['price']
+        quantity=product['quantity']
+        unit=product['unit']
+
         passport=''
         for p in products: 
             if p['id']==product['product_id']:
                 passport = p['passport']
+                
                 break
         info=''
         if product['set']:
             info = 'ZESTAW'
         if product['part_of_set']:
             info = 'CZĘŚĆ ZESTAWU'
-        table_rows.append([clientname,product['name'],'',passport,info])
+        table_rows.append([clientname,product['name'],passport,price,quantity,unit,status_id,status_date,delivery_date,info])
 #for row in table_rows:
 #    print(row)
 
